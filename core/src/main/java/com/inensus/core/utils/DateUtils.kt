8ea6b60cce5af8b0e76bfa93ca6604c26dd0bb6b
Daniel Mohns <daniel.mohns@posteo.de>
2025-05-16 13:49:35 +0200

    Fix more `ktlint` warnings

--- core/src/main/java/com/inensus/core/utils/DateUtils.kt
+++ core/src/main/java/com/inensus/core/utils/DateUtils.kt
@@ -6,8 +6,6 @@ import java.util.*
 object DateUtils {
     private const val DATE_FORMAT_DD_MM_YYYY = "dd.MM.yyyy"
     const val DATE_FORMAT_FULL = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-    const val DATE_PERIOD_FORMAT_dd_MMM = "dd MMM"
-    const val DATE_PERIOD_FORMAT_yyyy_MM_dd = "yyyy-MM-dd"
 
     fun convertDateToString(
         date: Date,
