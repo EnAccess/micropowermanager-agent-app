8799a3f7f59ec1b0c7d22736b15ae021fd39df51
Daniel Mohns <daniel.mohns@posteo.de>
2025-06-18 10:54:20 +0200

    Release `v1.6.0`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 31
         versionCode = 1
-        versionName = "1.5.3"
+        versionName = "1.6.0"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
 
